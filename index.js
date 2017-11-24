367327fc2a056654ec9fbd39d45f7901328db2ce
Sangram Dash <Sangram@Sangrams-MacBook-Pro.local>
2017-11-23 20:41:57 -0500

    change greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express(); // from this we create a single app express may contain many
 
 app.get('/',(req,res) => {
-	res.send({hi : "i am sangram"});
+	res.send({bye : "from sangram"});
 });
 const PORT= process.env.PORT || 5000;
 app.listen(PORT);
